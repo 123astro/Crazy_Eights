4fa8ec74c99c23062e0680ef76dcf2d666872f23
Keith Benjamin <kjb528i@yahoo.com>
2021-10-19 20:12:39 -0400

    autoPlayer -  look thru players  hand for suits and ranks first then play eights

--- src/com/company/actor/AutoPlayer.java
+++ src/com/company/actor/AutoPlayer.java
@@ -2,6 +2,7 @@ package com.company.actor;
 
 import com.company.crazyeights.Actor;
 import com.company.deck.Card;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,9 +23,14 @@ public class AutoPlayer implements Actor {
     @Override
     public int chooseCard(List<Card> cards, Card activeCard) {
         for (int i = 0; i < cards.size(); i++) {
-            if (cards.get(i).getSuit().equals(activeCard.getSuit()) ||
-                    cards.get(i).getRank() == activeCard.getRank())
+            if ((cards.get(i).getRank() == activeCard.getRank() && cards.get(i).getRank() != 8) ||
+                    (cards.get(i).getSuit().equals(activeCard.getSuit())) && cards.get(i).getRank() != 8){
+                return i + 1;}
+        }
+        for (int i = 0; i < cards.size(); i++){
+            if( cards.get(i).getRank() == 8){
                 return i + 1;
+            }
         }
         canPlay = false;
         return 1;
@@ -32,47 +38,45 @@ public class AutoPlayer implements Actor {
 
     @Override
     public int getAction() { // 1 = draw , 2 = play a card
-        if(!canPlay){
+        if (!canPlay) {
             canPlay = true;
             return 1;
         }
-        return 2 ;
+        return 2;
     }
 
     @Override
     public int setSuit(List<Card> cards) {
-        //int suit = (int) ((Math.random() * 4) + 1);
         Map<String, Integer> suitFreq = new HashMap<>();
-        for (int i = 0; i < cards.size(); i++)
-        {
-            if (suitFreq.containsKey(cards.get(i).getSuit()))
-            {
-                int value = suitFreq.get(cards.get(i).getSuit());
+        for (int i = 0; i < cards.size(); i++) {
+          //  System.out.println("Print getSuit " + cards.get(i).getSuit());
+            if (suitFreq.containsKey(cards.get(i).getSuit())) { // check to see if it is existing
+                int value = suitFreq.get(cards.get(i).getSuit()); // take the value for that key
+                //System.out.println("print value " + value + cards.get(i).getSuit());
                 value = value + 1;
-                suitFreq.put(cards.get(i).getSuit(), value);
-            } else
-            {
-               suitFreq.put(cards.get(i).getSuit(), 1);
+                suitFreq.put(cards.get(i).getSuit(), value); // put the key value in the map.
+            } else {
+                suitFreq.put(cards.get(i).getSuit(), 1); // use this for the first occurrence.
             }
         }
         String newSuit = null;
-        int max = Integer.MIN_VALUE;
-        for(String key : suitFreq.keySet()) {
-            if( suitFreq.get(key) > max ){
+        int max = Integer.MIN_VALUE; // min value is the lowest of the low
+        for (String key : suitFreq.keySet()) {
+            if (suitFreq.get(key) > max) {
                 max = suitFreq.get(key);
                 newSuit = key;
             }
         }
         assert newSuit != null;
         int suit = switch (newSuit) {
-            case "\u2667" ->  1;
-            case "\u2664" ->  2;
-            case "\u2665" ->  3;
-            case "\u2666" ->  4;
+            case "\u2667" -> 1;
+            case "\u2664" -> 2;
+            case "\u2665" -> 3;
+            case "\u2666" -> 4;
             default -> -1;
         };
 
-       return suit;
+        return suit;
     }
 
 }
--- src/readme.md
+++ src/readme.md
@@ -5,6 +5,8 @@ Added autoPlayer
 
 - When autoPlayer selects an eight, it will select 
 a suit based on the highest freq of suits in the hand.
+- Will look through the hand for matching suits and ranks that are not eights. If none are found, look for an eight to 
+  play. 
 
 Added reshuffle, at the start of the game, only draws an eight as the first card.
 
